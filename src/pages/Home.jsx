f110a6f5c102996db8f37fd9f3160100c26411ee
João Pedro Lopes <joaoplm.2006@gmail.com>
2026-08-24 10:59:43 -0300

    Update image dimensions in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ function Home() {
         <h1>Página Inicial</h1>
         <h2>Bem-vindo ao projeto Formulário de séries!</h2>
         <p>Gerencie séries assistidas de uma forma fácil e intuitiva.</p>
-        <img src="https://i.redd.it/bti4zwzgziac1.jpeg" style={{ maxWidth: '50%', height: '50px' }} />
+        <img src="https://i.redd.it/bti4zwzgziac1.jpeg" style={{ maxWidth: '10%', height: '10px' }} />
      </div>
     );
-}
+}z
 
 export default Home;
