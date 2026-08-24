e46fb494629bf92571fae35c393dfca37c650a69
João Pedro Lopes <joaoplm.2006@gmail.com>
2026-08-24 10:35:46 -0300

    Add image to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ function Home() {
         <h1>Página Inicial</h1>
         <h2>Bem-vindo ao projeto Formulário de séries!</h2>
         <p>Gerencie séries assistidas de uma forma fácil e intuitiva.</p>
+        <img src="https://i.redd.it/bti4zwzgziac1.jpeg"/>
      </div>
     );
 }
