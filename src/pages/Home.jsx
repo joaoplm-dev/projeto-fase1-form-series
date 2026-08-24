de7a5d64be93b3b078b0a0d7d775bea10f6d4e15
João Pedro Lopes <joaoplm.2006@gmail.com>
2026-08-24 11:18:36 -0300

    Replace empty paragraph with empty heading for spacing in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
         <h1>Página Inicial</h1>
         <h2>Bem-vindo ao projeto Formulário de séries!</h2>
         <p>Gerencie séries assistidas de uma forma fácil e intuitiva.</p>
-        <p></p>
+        <h1></h1>
         <img src="https://i.redd.it/bti4zwzgziac1.jpeg" style={{ maxWidth: '50%', height: 'auto' }} />
      </div>
     );
